142220890d2c7d6cf66315758eafa5cbb0b3131b
Kayc <kaycmendes@gmail.com>
2020-07-29 10:56:31 -0300

    remove import

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,19 +1,33 @@
-import React, { Component } from 'react';
 
 
 
-class Greeting extends Component {
+import React from 'react';
 
-  
-  render() {
+
+
+function Greeting(props) {
+
+  function dayNight(){
+    let day = props.time[0];
+    if(day < 12){
+      day = 'Morning'
+    }else if(day > 12){
+      day = 'Afternoon'
+    }else if(day > 18){
+      day = 'Night'
+    }
+    return day
+    
+  }dayNight()
+
+
     return (
       <>
         <div className="greeting">
-          <h1> Good (morning) (name)</h1>
+          <h1> Good {dayNight()} (name)</h1>
         </div>
       </>
     )
-  }
 
 
 }
