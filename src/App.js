727d824a4d955bb7a56da56c7f0205d62dc249f7
Kayc <kaycmendes@gmail.com>
2020-07-29 10:55:58 -0300

    center components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Clock from './components/Clock'
-import Greeting from './components/Greeting'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Clock/>
-      <Greeting/>
+   
     </div>
   );
 }
